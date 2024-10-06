7569a37fc2531c7c1349039b8e44dcf03822f891
Shady abdelaziz <95715375+Shady-Abdelaziz@users.noreply.github.com>
2024-10-06 16:22:15 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -1,28 +1,41 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
+import zipfile
+import os
+import gdown
 
-# Load models
+models_url = 'https://github.com/Shady-Abdelaziz/ECG-Classifications-Using-2-Models/raw/refs/heads/main/ECG%20Models.zip'
+
 @st.cache(allow_output_mutation=True)
 def load_models():
+    # Download the models.zip 
+    if not os.path.exists('ECG Models.zip'):
+        gdown.download(models_url, 'ECG Models.zip', quiet=False)
+
+    # Unzip the file
+    with zipfile.ZipFile('ECG Models.zip', 'r') as zip_ref:
+        zip_ref.extractall()
+
+    # Load the RandomForest model
     with open('rf_model.pkl', 'rb') as f:
         rf_model = pickle.load(f)
+
+    # Load the XGBoost model
     with open('xgb_model.pkl', 'rb') as f:
         xgb_model = pickle.load(f)
+
     return rf_model, xgb_model
 
 rf_model, xgb_model = load_models()
 
-# App title
+# Streamlit App Code
 st.title("ECG Classification using ML Models")
 
 # File uploader for test data
 uploaded_file = st.file_uploader("Upload CSV test file", type=["csv"])
 
 if uploaded_file is not None:
-    # Read the uploaded CSV file
     test_data = pd.read_csv(uploaded_file, header=None)
     X_test = test_data.iloc[:, :100]
 
